62283f1899acc490b71556ff94da414497e158d1
Baloch-Rasheed <balochrasheed888@gmail.com>
2025-11-06 23:33:58 +0500

    3-nov-2025

---
+++ binary_search_tree/node.py
@@ -0,0 +1,9 @@
+class Node:
+    right_side = None
+    left_side = None
+    def __init__(self, key):
+        self.key = key
+
+        # # Relations
+        # self.right_side = r
+        # self.left_side = l
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from sub_string_matching import sub_string_matching_algorithm
-from floyd_warshall import floyd_warshall_algorithm
+# from sub_string_matching import sub_string_matching_algorithm
+# from floyd_warshall import floyd_warshall_algorithm
+from binary_search_tree import node
 
 def main():
+    node1 = node.Node(434)
+    node2 = node.Node(300)
+    node3 = node.Node(500)
+
+    node1.left_side = node2
+    node1.right_side = node3
+    temp = node1
+    for i in range(2):
+        print(temp.key)
+        temp = temp.right_side
+
+
+
+    
+
     # txt = 'My name is Kamal Khan, and they call me Kamal'
     # ptrn = 'Kamal Khan'
 
@@ -9,18 +25,18 @@ def main():
 
     # print('results: ', result)
 
-    inf = float('inf')
-    graph = [
-        [0, 5, 2, inf],
-        [5, 0, 3, inf],
-        [2, 3, 0, 2],
-        [inf, inf, 2, 0]
-    ]
+    # inf = float('inf')
+    # graph = [
+    #     [0, 5, 2, inf],
+    #     [5, 0, 3, inf],
+    #     [2, 3, 0, 2],
+    #     [inf, inf, 2, 0]
+    # ]
 
-    result = floyd_warshall_algorithm(graph=graph)
+    # result = floyd_warshall_algorithm(graph)
 
-    for row in result:
-        print(row)
+    # for row in result:
+    #     print(row)
 
 
 
