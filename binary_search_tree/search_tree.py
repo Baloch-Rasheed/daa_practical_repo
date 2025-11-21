c0fa920f949a41e4f383dd39ec1de4f72fa5ff57
Baloch-Rasheed <balochrasheed888@gmail.com>
2025-11-21 13:03:12 +0500

    BST: Insertion and Traverse functions

--- binary_search_tree/search_tree.py
+++ binary_search_tree/search_tree.py
@@ -1,24 +1,65 @@
 from binary_search_tree.node import Node
 
 class BST:
+    
+    def init(self):
+        self._root = None
 
-    def insert(self, node : Node):
-        current_node = self._root
-        if current_node == None:
-            current_node = node
+    # def insert(self, key: int):
+    #     incoming_node = Node(key)
+    #     if self._root is None:
+    #         self._root = incoming_node
+    #     else:
+    #         current_node = self._root
+    #         while current_node:
+    #             if key < current_node.key:
+    #                 # The incomming value should in left side
+    #                 if current_node.left_side is None:
+    #                     current_node.left_side = incoming_node
+    #                     return
+    #                 else:
+    #                     current_node = current_node.left_side
+    #             else:
+    #                 # the equalant values should be in right
+    #                 if current_node.right_side is None:
+    #                     current_node.right_side = incoming_node
+    #                     return
+    #                 else:
+    #                     current_node = current_node.right_side
+    
+    def insert(self, key):
+        incoming_node = Node(key)
+        if self._root is None:
+            self._root = incoming_node
         else:
-            while current_node:
-                if node.key < current_node.key:
-                    if current_node.left_side == None:
-                        current_node.left_side = node
-                    else:
-                        current_node = current_node.left_side
+            self.__add_recursive(self._root, incoming_node)
 
-                elif node.key > current_node.key:
-                    if current_node.right_side == None:
-                        current_node.right_side = node
-                    else:
-                        current_node = current_node.right_side
-                else:
-                    # when node is equal to parent node then
-                    print("The Node is equal to Parent")
+        
+    def __add_recursive(self, parent: Node, incoming_node: Node):
+
+        if incoming_node.key < parent.key:
+            if parent.left_side is None:
+                parent.left_side = incoming_node
+            else:
+                self.__add_recursive(parent.left_side, incoming_node)
+        else:
+            if parent.right_side is None:
+                parent.right_side = incoming_node
+            else:
+                self.__add_recursive(parent.right_side, incoming_node)
+    
+    def traverse(self):
+        self.__treverse_recursive(self._root, 0)
+
+    def __treverse_recursive(self, parent: Node, layer: int):
+
+        if parent is None:
+            return
+
+        self.__treverse_recursive(parent.left_side, layer+1)
+        print(f"Key: {parent.key}, Layer: {layer}")
+        self.__treverse_recursive(parent.right_side, layer+1)
+
+
+    
+
--- main.py
+++ main.py
@@ -1,34 +1,52 @@
 # from sub_string_matching import sub_string_matching_algorithm
 # from floyd_warshall import floyd_warshall_algorithm
 # from binary_search_tree import node
-from linked_list.linked_list import LinkedList
-from linked_list.node import Node
+# from linked_list.linked_list import LinkedList
+# from linked_list.node import Node
+from binary_search_tree.search_tree import BST
 
 def main():
 
 
-    linkedList = LinkedList()
+    bst_al = BST()
 
-    node1 = Node(68)
-    node2 = Node(78)
-    node3 = Node(80)
-    node4 = Node(50)
+    bst_al.init()
 
-    linkedList.insert(node1)
-    linkedList.insert(node2)
-    linkedList.insert(node3)
-    linkedList.insert(node4)
+    bst_al.insert(6)
+    bst_al.insert(5)
+    bst_al.insert(2)
+    bst_al.insert(5.5)
+    bst_al.insert(6.5)
+    bst_al.insert(8)
+    bst_al.insert(7)
 
-    linkedList
+    bst_al.traverse()
 
-    print("Treverse dafault")
-    linkedList.treverse()
-    # print("Deleting 78...")
-    # linkedList.delete(68)
+
+
+
+    # linkedList = LinkedList()
+
+    # node1 = Node(68)
+    # node2 = Node(78)
+    # node3 = Node(80)
+    # node4 = Node(50)
+
+    # linkedList.insert(node1)
+    # linkedList.insert(node2)
+    # linkedList.insert(node3)
+    # linkedList.insert(node4)
+
+    # linkedList
+
+    # print("Treverse dafault")
     # linkedList.treverse()
-    print('Modifying 68 -> 43')
-    linkedList.modify(50, 43)
-    linkedList.treverse()
+    # # print("Deleting 78...")
+    # # linkedList.delete(68)
+    # # linkedList.treverse()
+    # print('Modifying 68 -> 43')
+    # linkedList.modify(50, 43)
+    # linkedList.treverse()
 
     
     
